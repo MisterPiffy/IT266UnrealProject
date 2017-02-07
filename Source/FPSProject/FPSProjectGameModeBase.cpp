fbc82b5c063c935ba2a9cccc704b8e3273f771d0
Zac <zlk.crispy@gmail.com>
2017-02-06 20:56:36 -0500

    Add files via upload

---
+++ Source/FPSProject/FPSProjectGameModeBase.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSProject.h"
+#include "FPSProjectGameModeBase.h"
+
+void AFPSProjectGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Welcome to FPSGameMode"));
+	}
+}
+
+
+
